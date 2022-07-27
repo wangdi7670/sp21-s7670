b81a438017ba38b65278f71e2a4f67be07643fe4
Wingd <wangdi7670@163.com>
2022-07-27 17:55:20 +0800

    done basics of Head and Branch

--- proj2/gitlet/Branch.java
+++ proj2/gitlet/Branch.java
@@ -3,6 +3,27 @@ package gitlet;
 /**
  * @author: Wingd
  * @date: 2022/7/27 17:37
+ *
+ * A branch is a pointer to a commit
  */
 public class Branch {
+    private String commitId;
+
+    private String branchName;
+
+    public String getBranchName() {
+        return branchName;
+    }
+
+    public void setBranchName(String branchName) {
+        this.branchName = branchName;
+    }
+
+    public String getCommitId() {
+        return commitId;
+    }
+
+    public void setCommitId(String commitId) {
+        this.commitId = commitId;
+    }
 }
--- proj2/gitlet/Head.java
+++ proj2/gitlet/Head.java
@@ -3,7 +3,17 @@ package gitlet;
 /**
  * @author: Wingd
  * @date: 2022/7/27 17:36
+ *
+ * The Head points to the active branch
  */
 public class Head {
+    private Branch activeBranch;
 
+    public Branch getActiveBranch() {
+        return activeBranch;
+    }
+
+    public void setActiveBranch(Branch activeBranch) {
+        this.activeBranch = activeBranch;
+    }
 }
